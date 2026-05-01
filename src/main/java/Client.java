f4292d99b955ae795f4397a78b76bb2054b64d0a
Marwan Mohamed <marwanmohamed2238@gmail.com>
2026-05-01 22:53:26 +0300

    init client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,10 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
 /**
  *
  * @author Marwan
@@ -5,6 +12,28 @@
 public class Client {
 
     public static void main(String[] args) {
-
+        try {
+            Socket socket = new Socket("localhost",20597);
+            
+            Scanner fromServer = new Scanner(socket.getInputStream());
+            PrintWriter fromClient = new PrintWriter(socket.getOutputStream());
+            Scanner fromConsole = new Scanner(System.in);
+            
+            String input, output;
+            
+            while(true) {
+                System.out.print("Enter your message: ");
+                input = fromConsole.nextLine();
+                
+                fromClient.println(input);
+                
+                System.out.println("Server: ");
+                output = fromServer.nextLine();
+                System.out.println(output);
+            }
+            
+        } catch (IOException ex) {
+            System.getLogger(Client.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
     }
 }
